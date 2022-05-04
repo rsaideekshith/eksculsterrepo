32a6010d5f72365fe45b81d11ca972136e2e429e
rsaideekshith <87260104+rsaideekshith@users.noreply.github.com>
2022-05-04 19:37:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,11 +5,4 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
-
-data "aws_availability_zones" "available" {}
-
-# Not required: currently used in conjunction with using
-# icanhazip.com to determine local workstation external IP
-# to open EC2 Security Group access to the Kubernetes cluster.
-# See workstation-external-ip.tf for additional information.
 provider "http" {}

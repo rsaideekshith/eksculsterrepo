b0b1cd7dc570a88e1059c6ebdc51565f60d5cc24
rsaideekshith <87260104+rsaideekshith@users.noreply.github.com>
2022-05-04 19:40:58 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -18,7 +18,6 @@ resource "aws_vpc" "demo" {
 resource "aws_subnet" "demo" {
   count = 2
 
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
   cidr_block              = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.demo.id
